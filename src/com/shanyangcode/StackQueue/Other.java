a1957d617ffd4e3c3bf01a44da24e96db16cd592
NuyoahCh <springoflotus@gmail.com>
2025-05-21 21:50:24 +0800

    实现优先级括号匹配

--- src/com/shanyangcode/StackQueue/Other.java
+++ src/com/shanyangcode/StackQueue/Other.java
@@ -48,15 +48,41 @@ public class Other {
 
 
     /**
-     给定一个只包括 '('，')'，'['，']'， '{'，'}' 的字符串 s ，括号的优先级排序由高到低依次为()，[]，{}，判断字符串是否有效。
-     有效字符串需满足：
-     左括号必须用相同类型的右括号闭合。
-     左括号必须以正确的顺序闭合。
-     每个右括号都有一个对应的相同类型的左括号。
-     内层括号的优先级不低于外层括号。
+     * 给定一个只包括 '('，')'，'['，']'， '{'，'}' 的字符串 s ，括号的优先级排序由高到低依次为()，[]，{}，判断字符串是否有效。
+     * 有效字符串需满足：
+     * 左括号必须用相同类型的右括号闭合。
+     * 左括号必须以正确的顺序闭合。
+     * 每个右括号都有一个对应的相同类型的左括号。
+     * 内层括号的优先级不低于外层括号。
      */
-
-
+    public boolean priIsValid(String s, String priorityOrder) {
+        if (s.length() % 2 == 1) {
+            return false;
+        }
+        Map<Character, Character> map = new HashMap<>();
+        map.put('(', ')');
+        map.put('[', ']');
+        map.put('{', '}');
+        Map<Character, Integer> priorityMap = new HashMap<>();
+        for (int i = 0; i < priorityOrder.length(); i++) {
+            priorityMap.put(priorityOrder.charAt(i), priorityOrder.length() - 1);
+        }
+        Deque<Character> stack = new LinkedList<>();
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            if (map.containsKey(c)) {
+                if (!stack.isEmpty() && priorityMap.get(c) < priorityMap.get(stack.peekLast())) {
+                    return false;
+                }
+                stack.offerLast(c);
+            } else if (!stack.isEmpty() & c == map.get(stack.peekLast())) {
+                stack.pollLast();
+            } else {
+                return false;
+            }
+        }
+        return stack.isEmpty();
+    }
 
 
     public static void main(String[] args) {

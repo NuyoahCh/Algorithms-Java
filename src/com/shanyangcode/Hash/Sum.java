19d67e15212a253fde837d14d2ab77a6ab9a2477
NuyoahCh <springoflotus@gmail.com>
2025-05-26 09:07:51 +0800

    实现两数之和

---
+++ src/com/shanyangcode/Hash/Sum.java
@@ -0,0 +1,29 @@
+package com.shanyangcode.Hash;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @program: Algorithms
+ * @author: NuyoahCh
+ * @create: 2025-05-26
+ * @description:
+ */
+public class Sum {
+
+    public static void main(String[] args) {
+
+    }
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{map.get(target - nums[i]), i};
+            } else {
+                map.put(nums[i], i);
+            }
+        }
+        return new int[]{};
+    }
+}
